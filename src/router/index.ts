fb56d94753a1b1240d76c88b7c40af622b89111a
zekaiz <chakcheung4565@outlook.com>
2021-07-15 16:42:20 +0800

    route init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,34 +1,33 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Main from '_c/main/main.vue'
-import Tools from './tools'
+import Tools from './routes/tools'
+import Tips from './routes/tips'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/library-info',
+    component: Main,
+    children: [
+      {
+        path: 'library-info',
+        name: 'library-info',
+        component: (): Promise<typeof import('*.vue')> =>
+          import(/* webpackChunkName: "index" */ '@/pages/index')
+      }
+    ]
+  },
+  ...Tips,
+  ...Tools,
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/404'
+  }
+]
 
 const router = createRouter({
   history: createWebHashHistory(), // hash模式：createWebHashHistory，history模式：createWebHistory
-  routes: [
-    {
-      path: '/404',
-      component: (): Promise<typeof import('*.vue')> =>
-        import(/* webpackChunkName: "index" */ '@/pages/default/404')
-    },
-    {
-      path: '/',
-      redirect: '/library-info',
-      component: Main,
-      children: [
-        {
-          path: 'library-info',
-          name: 'library-info',
-          component: (): Promise<typeof import('*.vue')> =>
-            import(/* webpackChunkName: "index" */ '@/pages/index/index.vue')
-        }
-      ]
-    },
-    ...Tools,
-    {
-      path: '/:pathMatch(.*)*',
-      redirect: '/404'
-    }
-  ]
+  routes
 })
 
 export default router
